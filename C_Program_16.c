c79cabdd9f5535cb1a21e3b6f649b0a657363b67
smharini18 <smharini18@gmail.com>
2026-06-07 22:13:01 +0530

    Create C_Program_16.c

---
+++ C_Program_16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, count = 0;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++)
+    {
+        if(n % i == 0)
+        {
+            count = count + 1;
+        }
+    }
+    if(count == 2)
+    {
+        printf("Prime");
+    }
+    else
+    {
+        printf("Not Prime");
+    }
+
+    return 0;
+}
